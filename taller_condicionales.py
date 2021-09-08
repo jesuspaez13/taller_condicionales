ae4c0b28e7d39aaeaee8c23fd41b0ca272cd9450
Jesus Paez <jpaez13@cuc.edu.co>
2021-09-08 16:20:09 -0500

    Problema 1

--- taller_condicionales.py
+++ taller_condicionales.py
@@ -4,3 +4,21 @@ Created on Wed Sep  8 15:03:17 2021
 
 @author: USER
 """
+# Problema 1
+numero_camisas = int(input('Digite la cantidad de camisas: '))
+if(numero_camisas >= 3):
+    precio_camisas = int(input('Digite el precio de las camisas: '))
+    valor_sin_descuento = numero_camisas * precio_camisas
+    print(f'El precio total sin descuento es: {valor_sin_descuento}')
+    descuento = valor_sin_descuento * 0.3
+    total = valor_sin_descuento - descuento
+    print(f'El valor total serían: {total}')
+elif(numero_camisas < 3 and numero_camisas > 0):
+    precio_camisas = int(input('Digite el precio de las camisas: '))
+    valor_sin_descuento = numero_camisas * precio_camisas
+    print(f'El precio total sin descuento es: {valor_sin_descuento}')
+    descuento = valor_sin_descuento * 0.1
+    total = valor_sin_descuento - descuento
+    print(f'El valor total serían: {total}')
+else:
+    print('El numero de camisas es equivocado')
